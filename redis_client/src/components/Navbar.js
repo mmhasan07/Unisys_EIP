5f0999eb03ca17ed65f3ef2b355355d88c4b43d7
Devank Gupta <gdevank22@gmail.com>
2024-05-03 17:04:50 +0530

    navbar changes

--- redis_client/src/components/Navbar.js
+++ redis_client/src/components/Navbar.js
@@ -1,56 +1,85 @@
-import React from 'react'
-import { Link, useNavigate } from 'react-router-dom'
-import unisysImg from '../assets/unisysImg.png'
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import unisysImg from "../assets/unisysImg.png";
+import { toast } from "react-toastify";
+
+const toastOptions = {
+  position: "top-right",
+  autoClose: 2000,
+  pauseOnHover: true,
+  draggable: true,
+  theme: "dark",
+};
 
 const Navbar = () => {
-    const navigate = useNavigate()
-    const logout = () => {
-        sessionStorage.removeItem(process.env.REACT_APP_CLIENT_KEY)
-        navigate("/login")
-      }
+  const navigate = useNavigate();
+  const logout = () => {
+    sessionStorage.removeItem(process.env.REACT_APP_CLIENT_KEY);
+    toast.success(`Logged out successfully`, toastOptions);
+    navigate("/login");
+  };
 
-    return (
-
-        <nav class="bg-white border-gray-200 dark:bg-gray-900">
-            <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                <Link to="/" class="flex items-center space-x-3 rtl:space-x-reverse">
-                    <img src={unisysImg} class="h-8" alt="Flowbite Logo" />
-                    <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Unisys EIP</span>
+  return (
+    <nav class="bg-white border-gray-200 dark:bg-gray-900">
+      <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <Link to="/" class="flex items-center space-x-3 rtl:space-x-reverse">
+          <img src={unisysImg} class="h-8" alt="Flowbite Logo" />
+          <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+            Unisys EIP
+          </span>
+        </Link>
+        <div class="hidden w-full md:block md:w-auto" id="navbar-default">
+          {sessionStorage.getItem(process.env.REACT_APP_CLIENT_KEY) && (
+            <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+              <li>
+                <Link
+                  to="/"
+                  class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
+                  aria-current="page"
+                >
+                  Home
                 </Link>
-                <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
-                    <span class="sr-only">Open main menu</span>
-                    <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
-                    </svg>
-                </button>
-                <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-                   {
-                    sessionStorage.getItem(process.env.REACT_APP_CLIENT_KEY) && (
-                        
-                        <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-                        <li>
-                            <Link to="/" class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</Link>
-                        </li>
-                        <li>
-                            <Link to="/features" class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Features</Link>
-                        </li>
-                        <li>
-                            <Link to="/features" class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">{sessionStorage.getItem("username")}</Link>
-                        </li>
-                        <li>
-                            <Link to="/features" class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">{sessionStorage.getItem("organization")}</Link>
-                        </li>
-                        <li>
-                            <a onClick={logout} href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Logout</a>
-                        </li>
-                    </ul>
-                    )
-                   }
+              </li>
+              <li>
+                <Link
+                  to="/features"
+                  class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
+                  aria-current="page"
+                >
+                  Features
+                </Link>
+              </li>
+              <li>
+                <div
+                  class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                  aria-current="page"
+                >
+                  {sessionStorage.getItem("username")}
                 </div>
-            </div>
-        </nav>
+              </li>
+              <li>
+                <div
+                  class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                  aria-current="page"
+                >
+                  {sessionStorage.getItem("organization")}
+                </div>
+              </li>
+              <li>
+                <a
+                  onClick={logout}
+                  href="#"
+                  class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
+                >
+                  Logout
+                </a>
+              </li>
+            </ul>
+          )}
+        </div>
+      </div>
+    </nav>
+  );
+};
 
-    )
-}
-
-export default Navbar
+export default Navbar;
--- redis_client/src/pages/Home.js
+++ redis_client/src/pages/Home.js
@@ -1,25 +1,27 @@
-import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
-import Data from '../components/Data';
-import { host } from '../utils/APIRoutes';
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import Data from "../components/Data";
+import { host } from "../utils/APIRoutes";
 
 function Home() {
   const navigate = useNavigate();
   const [filterChannels, setFilterChannels] = useState([]);
-  const [selectedFilterChannel, setSelectedFilterChannel] = useState('');
+  const [selectedFilterChannel, setSelectedFilterChannel] = useState("");
 
   useEffect(() => {
     async function getSubsChannels() {
       try {
         const resp = await axios.get(`${host}/api/channels/get`, {
           headers: {
-            Authorization: `Bearer ${sessionStorage.getItem(process.env.REACT_APP_CLIENT_KEY)}`,
+            Authorization: `Bearer ${sessionStorage.getItem(
+              process.env.REACT_APP_CLIENT_KEY
+            )}`,
           },
         });
         setFilterChannels(resp.data.subscribedChannels);
       } catch (error) {
-        console.error('Error fetching subscribed channels:', error);
+        console.error("Error fetching subscribed channels:", error);
       }
     }
     getSubsChannels();
@@ -32,13 +34,20 @@ function Home() {
   return (
     <>
       <div className="">
-        <div className="text-4xl w-[100vw] ml-auto mr-auto align-middle">Your Data</div>
-        <div className="text-2xl w-[100vw] ml-auto mr-auto align-middle">Your Data</div>
+        <div className="text-4xl w-[100vw] ml-auto mr-auto align-middle">
+          Your Data
+        </div>
         {/* Dropdown menu for selecting filter channel */}
-        <select value={selectedFilterChannel} onChange={handleFilterChange} className="mt-4 mb-8 p-2 rounded-lg border border-gray-300">
+        <select
+          value={selectedFilterChannel}
+          onChange={handleFilterChange}
+          className="mt-4 mb-8 p-2 rounded-lg border border-gray-300"
+        >
           <option value="">Select a channel</option>
           {filterChannels.map((channel, index) => (
-            <option key={index} value={channel}>{channel}</option>
+            <option key={index} value={channel}>
+              {channel}
+            </option>
           ))}
         </select>
         <Data selectedFilterChannel={selectedFilterChannel} />
